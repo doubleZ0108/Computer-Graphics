ba50ed78f361dc1b2cf2d39705ed7da2dd5cc83c
double Z <doubleZ0108@163.com>
2019-09-19 21:28:33 +0800

    绘制 折线

---
+++ OpenGL-study/LineStrip.cpp
@@ -0,0 +1,14 @@
+/*’€œﬂ*/
+void drawLineStrip()
+{
+	glColor3f(1.0, 1.0, 1.0);
+
+	glLineWidth(5.0);
+
+	glBegin(GL_LINE_STRIP);
+	glVertex3d(0.0f, 0.0f, 0.0f);
+	glVertex3d(0.2f, 0.2f, 0.2f);
+	glVertex3d(0.2f, 0.5f, 0.0f);
+	glVertex3d(0.4f, 0.7f, 0.0f);
+	glEnd();
+}
